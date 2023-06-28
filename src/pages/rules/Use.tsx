f0fef8aa102771ec6e100eac13b20370326f0e42
vignesh222002 <gtavignesh22042002@gmail.com>
2023-06-28 19:09:26 +0530

    end-commit

--- src/pages/rules/Use.tsx
+++ src/pages/rules/Use.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from '../../styles/rules/use/style.module.scss'
+import paraStyle from '../../styles/rules/use/_para.module.scss'
 
 function Use() {
   return (
     <>
         <div className={styles.head}>Use Rules Page</div>
         <div className={styles.para}>Use Paragraph</div>
+        <div className={paraStyle.para}>Show Para Style Line</div>
     </>
   )
 }
